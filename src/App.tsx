e0ca4370d0a731f6af427ebf85e234df2de10277
adrbervi <adrianbermudez21@gmail.com>
2025-05-27 22:10:10 -0600

    added the Product.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Product from "./componentes/User"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
     <h1>Hola mi proyecto final</h1>
+    <Product />
     </>
   )
 
